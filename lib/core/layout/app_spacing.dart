fd7d8d85f74b479ed0d8e6d8bc92d373d44e98fa
niassuh1 <galenwaros@gmail.com>
2023-02-24 15:27:55 +0300

    feat: Spacing, and text theme constants, hive_flutter and flutter_svg packages added

---
+++ lib/core/layout/app_spacing.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppSpacing {
+  // Horizontal spacing
+  static const kHorizontal1 = SizedBox(width: 2);
+  static const kHorizontal2 = SizedBox(width: 4);
+  static const kHorizontal3 = SizedBox(width: 6);
+  static const kHorizontal4 = SizedBox(width: 8);
+  static const kHorizontal5 = SizedBox(width: 10);
+  static const kHorizontal6 = SizedBox(width: 12);
+  static const kHorizontal7 = SizedBox(width: 14);
+  static const kHorizontal8 = SizedBox(width: 16);
+  static const kHorizontal9 = SizedBox(width: 20);
+  static const kHorizontal10 = SizedBox(width: 24);
+
+  // Vertical spacing
+  static const kVertical1 = SizedBox(height: 2);
+  static const kVertical2 = SizedBox(height: 4);
+  static const kVertical3 = SizedBox(height: 6);
+  static const kVertical4 = SizedBox(height: 8);
+  static const kVertical5 = SizedBox(height: 10);
+  static const kVertical6 = SizedBox(height: 12);
+  static const kVertical7 = SizedBox(height: 14);
+  static const kVertical8 = SizedBox(height: 16);
+  static const kVertical9 = SizedBox(height: 20);
+  static const kVertical0 = SizedBox(height: 24);
+}
---
+++ lib/core/layout/app_text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme {
+  static const TextStyle body1 = TextStyle(fontSize: 13);
+  static const TextStyle body2 = TextStyle(fontSize: 16);
+  static const TextStyle h1 = TextStyle(fontSize: 32);
+  static const TextStyle h2 = TextStyle(fontSize: 26);
+  static const TextStyle h3 = TextStyle(fontSize: 22);
+  static const TextStyle h4 = TextStyle(fontSize: 18);
+}
